2ecb024db3cb3d4f37ffc40877b3e9ca9df7b803
Samuel Elliott <samuel@fancy.org.uk>
2020-03-01 17:47:49 +0000

    Support changing server ports in environment variables

--- src/cli/tunnelserver.ts
+++ src/cli/tunnelserver.ts
@@ -9,9 +9,15 @@ import * as net from 'net';
 import * as path from 'path';
 import {promises as fs, unlinkSync} from 'fs';
 
-(async ({data_path, run_path}) => {
+(async ({
+    data_path, run_path,
+    http_port, https_port, httphttps_port, server_port, secureserver_port,
+}) => {
     await fs.writeFile(path.join(run_path, 'tunnel-server.pid'), process.pid, 'utf-8');
     let deleted_pid = false;
+    process.on('exit', () => {
+        if (!deleted_pid) unlinkSync(path.join(run_path, 'tunnel-server.pid')), deleted_pid = true;
+    });
 
     const tunnelserver = new TunnelServer();
 
@@ -39,7 +45,7 @@ import {promises as fs, unlinkSync} from 'fs';
 
     const http_service = await tunnelserver.addService(ServiceType.HTTP, DEFAULT_HTTP_SERVICE_IDENTIFIER, HttpService, {
         host: '::',
-        port: 9001,
+        port: http_port,
     });
 
     const http_serviceaddress = http_service.server.address() as net.AddressInfo;
@@ -50,7 +56,7 @@ import {promises as fs, unlinkSync} from 'fs';
     const https_service = await tunnelserver.addService(
         ServiceType.HTTPS, DEFAULT_HTTPS_SERVICE_IDENTIFIER, HttpsService, {
         host: '::',
-        port: 9002,
+        port: https_port,
     });
 
     https_service.hostname_regex = /\.hapserver-tunnel\.test$/;
@@ -63,7 +69,7 @@ import {promises as fs, unlinkSync} from 'fs';
 
     const httphttps_service = await tunnelserver.addService(ServiceType.HTTP_HTTPS, 0, HttpHttpsService, {
         host: '::',
-        port: 9003,
+        port: httphttps_port,
     });
 
     const httphttps_serviceaddress = httphttps_service.server.address() as net.AddressInfo;
@@ -75,7 +81,7 @@ import {promises as fs, unlinkSync} from 'fs';
 
     const server = await tunnelserver.createServer({
         host: '::',
-        port: 9000,
+        port: server_port,
     });
 
     const serveraddress = server.address() as net.AddressInfo;
@@ -106,7 +112,7 @@ import {promises as fs, unlinkSync} from 'fs';
         ])),
     }, {
         host: '::',
-        port: 9004,
+        port: secureserver_port,
     });
 
     const secureserveraddress = secureserver.address() as net.AddressInfo;
@@ -137,9 +143,6 @@ import {promises as fs, unlinkSync} from 'fs';
         await fs.unlink(path.join(run_path, 'tunnel-server.pid'));
         deleted_pid = true;
     });
-    process.on('exit', () => {
-        if (!deleted_pid) unlinkSync(path.join(run_path, 'tunnel-server.pid')), deleted_pid = true;
-    });
 
     // @ts-ignore
     global.tunnelserver = tunnelserver, global.http_service = http_service, global.https_service = https_service, global.httphttps_service = httphttps_service, global.server = server, global.secureserver = secureserver;
@@ -148,4 +151,10 @@ import {promises as fs, unlinkSync} from 'fs';
         path.resolve(__dirname, '..', '..', 'data'),
     run_path: process.argv[2] ? path.resolve(process.cwd(), process.argv[2]) :
         path.resolve(__dirname, '..', '..'),
+
+    http_port: parseInt(process.env.HTTP_SERVER_PORT || '9001'),
+    https_port: parseInt(process.env.HTTPS_SERVER_PORT || '9002'),
+    httphttps_port: parseInt(process.env.HTTPHTTPS_SERVER_PORT || '9003'),
+    server_port: parseInt(process.env.TUNNEL_SERVER_PORT || '9000'),
+    secureserver_port: parseInt(process.env.SECURE_TUNNEL_SERVER_PORT || '9004'),
 });
